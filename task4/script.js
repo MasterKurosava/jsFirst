cfc7ea9f29f0d766b111c35f593c02c1abb54ed8
Mr.Kurosava <81317891+MasterKurosava@users.noreply.github.com>
2021-05-24 03:17:13 +0600

    Update script.js

--- task4/script.js
+++ task4/script.js
@@ -1,19 +1,15 @@
 let link=document.getElementById('link');
 let message;
-let custom = document.createElement('span');
-let er=document.getElementById('error')
-custom.innerHTML='  <--- Это твое сообщение. Попробуй еще раз';
-console.log(custom)
+let er=document.getElementById('error');
 link.addEventListener('click',function(event){
+    er.textContent='';
     event.preventDefault();
     message = prompt('Введите свое сообщение');
-    console.log(message);
     if ((message=='') || (message==null)){
-        custom.textContent='Вы ничего не ввели! Попробуйте еще раз';
-        er.textContent=custom.textContent;
+        er.textContent='Вы ничего не ввели! Попробуйте еще раз';
     }
-    else{
-        link.textContent=message
-        link.parentElement.insertAdjacentElement('beforeend', custom)
+    else if(message!=''){
+        link.textContent=message;
+        er.textContent='Это твое сообщение. Попробуй еще раз'
     }
 })
